0ee45924149c5cc107934f3b7a99a84be905a3db
Lebackrobot <lebackman18@gmail.com>
2024-07-13 20:29:10 -0300

    feat: update client

--- apis/backend.js
+++ apis/backend.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
 const backend = {
-    sendMessage: async (payload) => {
+    sendMessage: async (payload, token) => {
+        const config = { headers: { 'Authorization': token, 'Content-Type': 'application/json' }}
+
         try {
-            await axios.post('http://localhost:4000/chat', payload)
+            await axios.post('http://localhost:4000/auth/messages', payload, config)
         }
 
         catch(error) {
--- client.js
+++ client.js
@@ -6,8 +6,11 @@ import backend from './apis/backend.js'
 
 
 const nickname = getRandomNickname()
-const endpoint = `http://localhost:4000/chat?nickname=${nickname}`
-const eventSource = new EventSource(endpoint)
+const endpoint = `http://localhost:4000/auth/event-stream`
+const eventSource = new EventSource(endpoint, {
+    headers: { authorization: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTcxNDk3MTc3NSwiZXhwIjoxNzE1MTQ0NTc1fQ.dsbHBtaLuMTJD__DBuuiqpxp8CezUb4OgO-kAuyGOWI' }
+})
+
 
 serverEvents(eventSource)
 
@@ -15,4 +18,3 @@ serverEvents(eventSource)
 consoleInterface.on('line', message => {
     backend.sendMessage({ nickname, message })
 })
-
