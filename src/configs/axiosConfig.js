a5736882d38ca445deac084d758182ab7c14add4
Lebackrobot <lebackman18@gmail.com>
2024-08-11 20:02:50 -0300

    feat: failover chatControllerBackup

--- src/configs/axiosConfig.js
+++ src/configs/axiosConfig.js
@@ -1,9 +1,13 @@
 const urlBase = 'https://bla-bla-furg-backend.onrender.com'
+const urlBaseBackup = 'https://bla-bla-furg-backend-backup.onrender.com'
+
 const eventStreamUrlBase = 'https://bla-bla-furg-backend.onrender.com'
+const eventStreamUrlBackup = 'https://bla-bla-furg-backend-backup.onrender.com'
+
 const headers = { 
     'Content-Type': 'application/json', 
     'Authorization': `${window.localStorage.getItem('token')}`
 }
 
 
-export { headers, urlBase, eventStreamUrlBase }
+export { headers, urlBase, urlBaseBackup, eventStreamUrlBackup, eventStreamUrlBase }
